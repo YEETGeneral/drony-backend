86606998aed371acd06cb74d0f86635edf612e50
Maksymilian Karol Mazur <81038515+YEETGeneral@users.noreply.github.com>
2025-11-15 15:57:16 +0100

    Add code template and .gitignore

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const PORT = 3003
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Serwer działa!')
+})
+
+app.post('/add-drone', (req, res) => {
+  const { latitude, longitude, azimuth, direction } = req.body
+  console.log(`Dodano drona: Lat: ${latitude}, Lon: ${longitude}, Azimuth: ${azimuth}, Direction: ${direction}`)
+  res.status(201).send('Dron dodany pomyślnie')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is Listening on ${PORT}`)
+})
